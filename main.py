a78ccd364c4f6bb2c41309c3958635e890af1551
Winston <winstonyeo99@yahoo.com>
2020-02-14 01:40:40 +0800

    🚧 Structure rough code outline

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import datetime
+
+
+def get_data_from_active_sg(date, time):
+    return 0
+
+
+def get_data_from_pa(date, time):
+    return 0
+
+
 def main():
-    pass
+    date = datetime.datetime(2020, 2, 14)
+    time = datetime.time(15, 00, 00)
+    active_sg_slots = get_data_from_active_sg(date, time)
+    pa_slots = get_data_from_pa(date, time)
+    print(active_sg_slots, "/n/n", pa_slots)
 
 
 if __name__ == "__main__":
