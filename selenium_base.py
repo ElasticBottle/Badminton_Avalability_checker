3d2a942e129fe03ceb74eb23e0fc2ac087d0ffe7
Winston <winstonyeo99@yahoo.com>
2020-02-16 14:52:02 +0800

    🔥 Remove code so as to reduce coupling between one_pa and active_sg file

--- selenium_base.py
+++ selenium_base.py
@@ -35,66 +35,6 @@ class SeleniumBase(ABC):
         driver.get(self.baseURL)
         return driver
 
-    @staticmethod
-    def search_for_court_timings(self, driver, day, num_badminton_court, delay):
-        self._click_date(driver, day)
-        print("reached new date")
-
-        all_available_timing = dict()
-        driver.implicitly_wait(delay)
-
-        # ? Add multi threading to launch multiple web browsers at once and speed up search
-        for i in range(num_badminton_court):
-            court_name, court_timing = self._get_timing_for_court_loc(driver)
-            all_available_timing[court_name] = court_timing
-            self._go_to_court_loc(driver, i + 1)
-            driver.implicitly_wait(delay)
-        court_name, court_timing = self._get_timing_for_court_loc(driver)
-        all_available_timing[court_name] = court_timing
-
-        return all_available_timing
-
-    @abstractmethod
-    def _login(self, driver):
-        """
-        Logs into the website to perform scraping
-
-         Args:
-            driver (WebDriver): Contains either firefox or chrome webdriver.
-                Needed for use in interacting with the webpage that we want to log into
-        """
-        raise NotImplementedError
-
-    @abstractmethod
-    def _get_right_date(self, driver, day):
-        """
-        Searches through the list of available dates and attempts to click the day specified if possible
-
-        Args:
-            driver (WebDriver): Contains either firefox or chrome webdriver.
-                Needed for use in interacting with the webpages
-            day (int): specifies the day of the month that is to be checked for badminton court availability
-        
-        Return:
-            Bool: True for a successful click on the specified date, False otherwise
-        """
-        raise NotImplementedError
-
-    @abstractmethod
-    def _click_date(self, driver, day):
-        """
-        Clicks the appropriate date on the one_pa badminton website
-
-        Args:
-            driver (WebDriver): Contains either firefox or chrome webdriver.
-                Needed for use in interacting with the webpages
-            day (int): specifies the day of the month that is to be checked for badminton court availability
-        
-        Return:
-            Bool: True for a successful click on the specified date, False otherwise
-        """
-        return NotImplementedError
-
     @abstractmethod
     def _get_court_loc_name(self, driver):
         """
@@ -111,35 +51,6 @@ class SeleniumBase(ABC):
         raise NotImplementedError
 
     @abstractmethod
-    def _get_timing_structure_at_court_loc(self, driver):
-        """
-        Retrieves the timing structure at a particular court location
-
-        Args:
-            driver (WebDriver): Contains either firefox or chrome webdriver.
-                driver should be set to the badminton booking page of the court location that timing is to be retrieved for.
-        
-        Returns:
-            list<string>: A collection of the timings
-        """
-        raise NotImplementedError
-
-    @abstractmethod
-    def _get_available_courts_at_court_loc(self, driver):
-        """
-        Finds the timing index of the available courts at the particular court location
-
-        Args:
-            driver (WebDriver): Contains either firefox or chrome webdriver.
-                driver should be set to the badminton booking page of the court location that timing is to be retrieved for.
-        
-        Returns:
-            list<string>: A collection of the court timing indexes that is available.
-                Timing timing to be retrieved from _get_timing_structure_at_court_loc() function.
-        """
-        raise NotImplementedError
-
-    @abstractmethod
     def _get_timing_for_court_loc(self, driver):
         """
         Finds the name, timing structure, and available court of the court location
@@ -152,22 +63,34 @@ class SeleniumBase(ABC):
             string: Contains the name of the court location
             list<string>: Containing the timings whihc are available at the court location
         """
-        court_name = self._get_court_loc_name(driver)
-        court_timings = self._get_timing_structure_at_court_loc(driver)
-        court_available_slots = self._get_available_courts_at_court_loc(driver)
-        court_available_timings = [court_timings[int(i)] for i in court_available_slots]
-        print("Available timings at", court_name, ":\n", court_available_timings)
-        return court_name, court_available_timings
+        raise NotImplementedError
 
-    @abstractmethod
-    def _go_to_court_loc(self, driver, court_loc_to_check):
+
+class SeleniumDateHelper:
+    def _get_right_date(self, driver, day):
         """
-        Changes the badminton booking page for a particular court location that @param driver is at to the badminton booking page for another court location
+        Searches through the list of available dates and attempts to click the day specified if possible
 
         Args:
             driver (WebDriver): Contains either firefox or chrome webdriver.
-                driver should be set to the badminton booking page of any court location
-            court_loc_to_check (int): The index of the particular court location that you want to navigate too.
-                court_loc_to_check should be between 0 - 75 
+                Needed for use in interacting with the webpages
+            day (int): specifies the day of the month that is to be checked for badminton court availability
+        
+        Return:
+            Bool: True for a successful click on the specified date, False otherwise
         """
         raise NotImplementedError
+
+    def _click_date(self, driver, day):
+        """
+        Clicks the appropriate date on the one_pa badminton website
+
+        Args:
+            driver (WebDriver): Contains either firefox or chrome webdriver.
+                Needed for use in interacting with the webpages
+            day (int): specifies the day of the month that is to be checked for badminton court availability
+        
+        Return:
+            Bool: True for a successful click on the specified date, False otherwise
+        """
+        return NotImplementedError
